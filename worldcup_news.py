c46689b91b6539ecefd07d6862eacf3d6228a4be
sulavlearns <sulavmaharjan@students.federation.edu.au>
2023-02-03 10:40:32 +0545

    first commit

---
+++ worldcup_news.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect("worldup_articles.db")
+cursor = conn.cursor()
+
+# Create the table to store the data
+cursor.execute("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, title TEXT, headers TEXT, contents TEXT)")
+
+# Scrape the text data from the news article
+url = "https://olympics.com/en/news/fifa-world-cup-2022-records-and-stats"
+html = requests.get(url).text
+soup = BeautifulSoup(html, "html.parser")
+
+# Extract the title and content of the article
+title = soup.find("h1").text
+headers = [header.text for header in soup.find_all("h1, h2")]
+header_contents = [header.find_next("p").text for header in soup.find_all("h1, h2")]
+paragraphs = soup.find_all("p")
+content = ' '.join([p.text for p in paragraphs])
+
+# Store the data in the database
+try:
+    cursor.execute("ALTER TABLE articles ADD COLUMN contents TEXT")
+except sqlite3.OperationalError:
+    pass
+cursor.execute("INSERT INTO articles (title, headers, contents) VALUES (?,?,?)", (title, ' '.join(headers), content))
+
+# Close the connection
+conn.commit()
+conn.close()
